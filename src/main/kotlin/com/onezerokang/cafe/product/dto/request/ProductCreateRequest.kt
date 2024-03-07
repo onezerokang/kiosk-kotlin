d30458918050a6551a4f27018a879540bc87495e
Wonyoung Kang <onezerokang@gmail.com>
2024-03-07 16:35:03 +0900

    Fix: ProductCreateRequest 유효성 검사

--- src/main/kotlin/com/onezerokang/cafe/product/dto/request/ProductCreateRequest.kt
+++ src/main/kotlin/com/onezerokang/cafe/product/dto/request/ProductCreateRequest.kt
@@ -5,19 +5,28 @@ import com.onezerokang.cafe.member.domain.Member
 import com.onezerokang.cafe.product.domain.Product
 import com.onezerokang.cafe.product.domain.ProductCategory
 import com.onezerokang.cafe.product.domain.ProductSize
+import jakarta.validation.constraints.FutureOrPresent
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Positive
 import java.time.LocalDate
 
 data class ProductCreateRequest(
+    @field:NotBlank
     val name: String,
 
+    @field:NotBlank
     val description: String,
 
+    @field:NotBlank
     val barcode: String,
 
+    @field:Positive
     val salePrice: Int,
 
+    @field:Positive
     val originalPrice: Int,
 
+    @field:FutureOrPresent
     val expirationDate: LocalDate,
 
     val category: ProductCategory,
