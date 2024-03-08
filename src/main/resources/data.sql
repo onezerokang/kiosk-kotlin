b65e31f6961de8752b93f4c66ea8b5de0ad01c04
Wonyoung Kang <onezerokang@gmail.com>
2024-03-08 13:52:23 +0900

    Chore: 개발 환경 용 data.sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+insert into member(phone_number, password) value('01012341234', '$2a$10$RL9AarJZqw96Ru4KTGhkFOql.RpXclB4b6qg3soA246xm25AiKBIi');
+
+insert into product(name, initial_name, description, barcode, sale_price, original_price, expiration_date, category, size, status, member_id)
+value('아메리카노', 'ㅇㅁㄹㅋㄴ', '아이스', '1234', 4000, 5000, '2024-12-31', 'DRINK', 'LARGE', 'SELLING', 1);
+
+insert into product(name, initial_name, description, barcode, sale_price, original_price, expiration_date, category, size, status, member_id)
+value('카푸치노', 'ㅋㅍㅊㄴ', '아이스', '1234', 4000, 5000, '2024-12-31', 'DRINK', 'LARGE', 'SELLING', 1);
+
+insert into product(name, initial_name, description, barcode, sale_price, original_price, expiration_date, category, size, status, member_id)
+value('밤양갱', 'ㅂㅇㄱ', '아이스', '1234', 4000, 5000, '2024-12-31', 'DRINK', 'LARGE', 'SELLING', 1);
+
+insert into product(name, initial_name, description, barcode, sale_price, original_price, expiration_date, category, size, status, member_id)
+value('수제쿠키', 'ㅅㅈㅋㅋ', '아이스', '1234', 4000, 5000, '2024-12-31', 'DRINK', 'LARGE', 'SELLING', 1);
+
+insert into product(name, initial_name, description, barcode, sale_price, original_price, expiration_date, category, size, status, member_id)
+value('롤케이크', 'ㄹㅋㅇㅋ', '아이스', '1234', 4000, 5000, '2024-12-31', 'DRINK', 'LARGE', 'SELLING', 1);
+
+insert into product(name, initial_name, description, barcode, sale_price, original_price, expiration_date, category, size, status, member_id)
+value('푸딩', 'ㅍㄷ', '아이스', '1234', 4000, 5000, '2024-12-31', 'DRINK', 'LARGE', 'SELLING', 1);
